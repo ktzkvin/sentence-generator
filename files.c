651636ef4c21a3b4f8a3240866e561e3c4faedeb
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-08 11:44:31 +0100

    Updates files.c

--- files.c
+++ files.c
@@ -6,31 +6,35 @@
 //Je veux que la fonction OpenFile() retourne un tableau de caractères
 int OpenFile()
 {
-    FILE *fileptr;
-    int *text = malloc(MAX_BUFFER * sizeof(char));
-    int i = 0;
+        FILE *fileptr;
 
     // Ouverture du fichier
     fileptr = fopen("dico_10_lignes.txt", "r");
 
     // Problème lors de la lecture du fichier
-    if (fileptr == NULL)
-    {
+    if (fileptr == NULL) {
         printf("Erreur d'ouverture du fichier\n");
-        return 1;
     }
 
-    // Lecture du fichier caractère par caractère
-    int c;
-    while ((c = fgetc(fileptr)) != EOF){
-        printf("%c", c);
-        text[i] = c;
-        i++;
+    // Lecture du fichier + stockage dans un tableau de caractères 2D
+    // fgets() pour lire une ligne du fichier
+    // strtok() pour découper la ligne en plusieurs parties
+    // strcpy() pour copier la première partie dans le tableau de caractères 2D
+    char text[100][100];
+    if (fileptr != NULL) {
+        char line[100];
+        char *token;
+        int i = 0;
+        while (fgets(line, sizeof(line), fileptr) != NULL) {
+            token = strtok(line, "\t");
+            strcpy(text[i], token);
+            i++;
+        }
+        for (int j = 0; j < i; j++) {
+            printf("%s\n", text[j]);
+        }
     }
 
     // Fermeture du fichier
     fclose(fileptr);
-    // Retourne le tableau de caractères
-    //return text;
-    return 0;
 }
