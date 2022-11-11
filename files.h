d01e8c632e4cb6fd1a95226ee53a3e6775e39d89
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-11 23:55:16 +0100

    Update files.h

--- files.h
+++ files.h
@@ -1,5 +1,7 @@
 #ifndef SENTENCE_GENERATOR_FILES_H
 #define SENTENCE_GENERATOR_FILES_H
+#include "struct.h"
+#include "files.h"
 
 // Créer une fonction qui ouvre un fichier, l'extrait et le stocke dans un tableau de caractères
 int OpenFile();
