268632268b3da03c4700a02236fad88aca90af6b
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-11 23:54:47 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,29 +1,34 @@
 #ifndef SENTENCE_GENERATOR_STRUCT_H
 #define SENTENCE_GENERATOR_STRUCT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+#include "files.h"
 
 typedef struct s_letter_node t_letter_node, *p_letter_node ;
 
 typedef struct s_cell
 {
-    p_letter_node son ;
-    struct s_cell * next ;
+    p_letter_node p_node ;  // Lettre du fils
+    struct s_cell * next ;  // Lettre à droite = frère
 } t_cell, *p_cell ;
-
-typedef struct s_std_list
-{
-    p_cell head ;
-} t_std_list ;
 
 struct s_letter_node
 {
     char letter ;
-    t_std_list sons ;
+    p_cell son ;
 };
 
-typedef struct root
+typedef struct s_tree
 {
-    p_letter_node a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z ;
-} t_root, *p_root ;
+    t_letter_node *root;
+} t_tree, *p_tree;
+
+p_letter_node create_node(p_letter_node node, char letter);
+p_cell create_son(p_cell son, char letter);
+void add_son(p_letter_node node, char letter);
+void add_word(p_letter_node node, char * word);
+void display_tree(p_letter_node node, char * word);
 
 
 #endif //SENTENCE_GENERATOR_STRUCT_H
