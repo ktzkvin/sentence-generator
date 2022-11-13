ded828e834ebec6810f9990d4079ce16aae73135
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-13 22:13:48 +0100

    Update files.h

--- files.h
+++ files.h
@@ -10,7 +10,7 @@ void OpenFile_2();
 int **extract_column_1();
 int **extract_column_2();
 int **extract_column_3();
-void extract_line(char *, int);
+void extract_line(char *);
 void decompose_line(char *, char **);
 int nature_line(char **);
 
