1b2ce5d2158b836397277d0a094d28c4f463bfde
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-13 22:15:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -48,14 +48,14 @@ int main()
         tab_off[i] = malloc(3 * sizeof(char));
     }
     int number_line = 6;  // Numéro de la ligne du fichier
-    extract_line(temp_ligne, number_line);  // Extraction de la ligne
+    extract_line(temp_ligne);  // Extraction de la ligne
     printf("\n--------------\nLigne numero %d :\n%s", number_line, temp_ligne);
     printf("--------------\n");
     decompose_line(temp_ligne, tab_off);  // temp_ligne = tableau temporaire contenant la ligne, tab_off = tableau séparant les 3 types de la ligne
     printf("Separation :\n");
     printf("tab_off[0] = %s\n", tab_off[0]);
     printf("tab_off[1] = %s\n", tab_off[1]);
-    printf("tab_off[2] = %s", tab_off[2]);
+    printf("tab_off[2] = %s\n", tab_off[2]);
     printf("--------------\n");
     if (nature_line(tab_off) == 1){
         printf("Type : Verbe\n");
@@ -68,12 +68,9 @@ int main()
     }
 
 
-    printf("\n");
-
     // Création de l'arbre avec le dico_10_lignes.txt
     // Création de l'arbre des verbes :
     t_tree tree_verbs = create_tree();
-    printf("-%c-", tree_verbs.root->letter);
     p_letter_node tmp = tree_verbs.root;
     // -> vérifie le type de la ligne tab_off[2] si = Ver
     // -> ajoute la forme de base dans l'arbre (= tab_off[1])
@@ -81,22 +78,29 @@ int main()
     // -> affiche l'arbre
 
     number_line = 11;  // Numéro de la ligne du fichier
-    extract_line(temp_ligne, number_line);  // Extraction de la ligne
+    extract_line(temp_ligne);  // Extraction de la ligne
     printf("\n--------------\nLigne numero %d :\n%s", number_line, temp_ligne);
     printf("--------------\n");
     decompose_line(temp_ligne, tab_off);  // temp_ligne = tableau temporaire contenant la ligne, tab_off = tableau séparant les 3 types de la ligne
     printf("Separation :\n");
     printf("tab_off[0] = %s\n", tab_off[0]);
     printf("tab_off[1] = %s\n", tab_off[1]);
-    printf("tab_off[2] = %s", tab_off[2]);
+    printf("tab_off[2] = %s\n", tab_off[2]);
     printf("--------------\n");
-
+    if (nature_line(tab_off) == 1){
+        printf("Type : Verbe\n");
+    } else if (nature_line(tab_off) == 2){
+        printf("Type : Nom\n");
+    } else if (nature_line(tab_off) == 3){
+        printf("Type : Adjectif\n");
+    } else {
+        printf("Type : Inconnu\n");
+    }
 
     add_word(tmp, tab_off[1]);
-    // -> affiche l'arbre
     printf("\n");
 
-
+    parcourir_le_dico(tree_verbs.root, 1);
 
 
 
