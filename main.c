1cb69513dc15cfef9b6b36c0524e28b2fa4ea896
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-20 23:30:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "struct.c"
 #include "files.c"
-#include <time.h>
+#include "struct.h"
+#include "files.h"
 
 
 int main()
 {
-    /*
     char *temp_ligne = malloc(100 * sizeof(char));  // Création d'un tableau temporaire pour enregistrer une ligne
     char *temp_ligne_2 = malloc(100 * sizeof(char));
     char *temp_ligne_3 = malloc(100 * sizeof(char));
@@ -19,9 +20,8 @@ int main()
         tab_off[i] = malloc(3 * sizeof(char));
     }
 
-    int nombreAleatoire;
-    int verif = 0;
-    int a = 0;
+    // Recherche aléatoire d'une ligne dans le dictionnaire
+    int nombreAleatoire, verif = 0, a = 0;
     const int MAX = 287975, MIN = 1;
     srand(time(NULL));
     char *tab_column_3;  // Création d'un tableau pour séparer les 3 types d'une ligne dans un tableau en 3 lignes
@@ -49,7 +49,7 @@ int main()
     printf("%s ", tab_off[0]);
 
     // --------------------- //  nature : Adj
-    while ((nature_line(tab_off) != 3) && verif != 1) {
+    while ((nature_line(tab_off) != 3)) {
         a = rand() + rand()*(1+MAX);  // Génération d'un nombre aléatoire : rand() génère un nombre aléatoire entre 0 et 32767, on le multiplie par 2 pour avoir un nombre entre 0 et 65534, on ajoute 1 pour avoir un nombre entre 1 et 65535, on multiplie par MAX pour avoir un nombre entre 1 et MAX
         if (a < 0){
             a=a*-1;
@@ -57,8 +57,8 @@ int main()
         nombreAleatoire = a%MAX;
         extract_line2(temp_ligne, nombreAleatoire);
         decompose_line(temp_ligne, tab_off, temp_ligne_2);
-        verif = adj_ligne(tab_off[2], verif_tab);
-        printf("[[%d]] [%d] ", nature_line(tab_off), verif);
+        //verif = adj_ligne(tab_off[2], verif_tab);
+        //printf("[[%d]] [%d] ", nature_line(tab_off), verif);
     }
     printf("%s ", tab_off[0]);
 
@@ -86,14 +86,10 @@ int main()
     }
     article_line(temp_ligne_2, tab_column_3, 0);  // Vérifier le genre et le nombre
     printf("%s \n", tab_off[0]);
-    */
-
-    t_tree Verbes;
-    parcourir_le_dico(Verbes.root);
 
 
-
-
+    // --------------------- //  Création des arbres
+    parcourir_le_dico();
 
 
     return 0;
