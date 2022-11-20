f1d61a04dfb024bbba7f453aef6dfc1cd5347c12
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-20 23:25:42 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,34 +1,31 @@
 #ifndef SENTENCE_GENERATOR_STRUCT_H
 #define SENTENCE_GENERATOR_STRUCT_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "struct.h"
-#include "files.h"
 
 typedef struct s_letter_node t_letter_node, *p_letter_node ;
 
-typedef struct s_cell
+typedef struct s_cell  // Structure d'une cellule de la liste chainée
 {
     p_letter_node p_node ;  // Lettre du fils
     struct s_cell * next ;  // Lettre à droite = frère
 } t_cell, *p_cell ;
 
-struct s_letter_node
+struct s_letter_node  // Structure d'un noeud de l'arbree
 {
-    char letter ;
-    p_cell son ;
+    char letter ;  // Lettre du noeud
+    p_cell son ;  // Liste chainée des fils
 };
 
-typedef struct s_tree
+typedef struct s_tree  // Structure de l'arbre
 {
-    t_letter_node *root;
+    t_letter_node *root;  // Racine de l'arbre
 } t_tree, *p_tree;
 
-p_letter_node create_node(p_letter_node node, char letter);
-p_cell create_son(p_cell son, char letter);
-void add_son(p_letter_node node, char letter);
-void add_word(p_letter_node node, char * word);
-void display_tree(t_tree);
+p_letter_node create_node(p_letter_node, char);  // Créer un noeud
+p_cell create_son(p_cell, char);  // Créer un fils
+void add_son(p_letter_node, char);  // Ajoute un fils à un noeud
+void add_word(p_letter_node, char*);  // Ajoute un mot à l'arbre
+void display_tree(t_tree);  // Affiche l'arbre
+p_cell verif_present(p_letter_node, char);  // Vérifie si une lettre est présente parmi les fils d'un noeud
 
 
 #endif //SENTENCE_GENERATOR_STRUCT_H
