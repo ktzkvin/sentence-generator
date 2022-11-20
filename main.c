15948caa19188914c1197d774da28aef681b2989
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-20 19:02:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,11 @@
 
 int main()
 {
+    /*
     char *temp_ligne = malloc(100 * sizeof(char));  // Création d'un tableau temporaire pour enregistrer une ligne
-    char *temp_ligne_2 = malloc(100 * sizeof(char));  // Création d'un tableau temporaire pour enregistrer une ligne
-
+    char *temp_ligne_2 = malloc(100 * sizeof(char));
+    char *temp_ligne_3 = malloc(100 * sizeof(char));
+    char *verif_tab = malloc(100 * sizeof(char));
 
     char **tab_off = malloc(100 * sizeof(char));  // Création d'un tableau pour séparer les 3 types d'une ligne dans un tableau en 3 lignes
     for (int i = 0; i < 100; i++) {
@@ -18,7 +20,9 @@ int main()
     }
 
     int nombreAleatoire;
-    const int MAX = 287976, MIN = 1;
+    int verif = 0;
+    int a = 0;
+    const int MAX = 287975, MIN = 1;
     srand(time(NULL));
     char *tab_column_3;  // Création d'un tableau pour séparer les 3 types d'une ligne dans un tableau en 3 lignes
     for (int i = 0; i < 100; i++) {
@@ -27,35 +31,65 @@ int main()
 
     printf("---------------------\n");
     printf("Generation numero 1 : \n");
-    while (nature_line(tab_off) != 2) {
-        nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN;
-        extract_line2(temp_ligne, nombreAleatoire);
 
+    // --------------------- //  nature : Nom
+    while (nature_line(tab_off) != 2) {
+        a = rand() + rand()*(1+MAX);  // Génération d'un nombre aléatoire : rand() génère un nombre aléatoire entre 0 et 32767, on le multiplie par 2 pour avoir un nombre entre 0 et 65534, on ajoute 1 pour avoir un nombre entre 1 et 65535, on multiplie par MAX pour avoir un nombre entre 1 et MAX
+        if (a < 0){
+            a=a*-1;
+        }
+        nombreAleatoire = a%MAX;
+        extract_line2(temp_ligne, nombreAleatoire);
         decompose_line(temp_ligne, tab_off, temp_ligne_2);
+        // Récupérer le genre et le nombre pour le comparer avec l'adjectif et le verbe
+        strcpy(verif_tab, tab_off[2]);
+        //printf("%s \n", verif_tab);
     }
     article_line(temp_ligne_2, tab_column_3, 1);  // Vérifier le genre et le nombre pour afficher le bon article
     printf("%s ", tab_off[0]);
 
-    while (nature_line(tab_off) != 3) {
-        nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN;
+    // --------------------- //  nature : Adj
+    while ((nature_line(tab_off) != 3) && verif != 1) {
+        a = rand() + rand()*(1+MAX);  // Génération d'un nombre aléatoire : rand() génère un nombre aléatoire entre 0 et 32767, on le multiplie par 2 pour avoir un nombre entre 0 et 65534, on ajoute 1 pour avoir un nombre entre 1 et 65535, on multiplie par MAX pour avoir un nombre entre 1 et MAX
+        if (a < 0){
+            a=a*-1;
+        }
+        nombreAleatoire = a%MAX;
         extract_line2(temp_ligne, nombreAleatoire);
         decompose_line(temp_ligne, tab_off, temp_ligne_2);
+        verif = adj_ligne(tab_off[2], verif_tab);
+        printf("[[%d]] [%d] ", nature_line(tab_off), verif);
     }
     printf("%s ", tab_off[0]);
+
+    // --------------------- // nature : Verbe
     while (nature_line(tab_off) != 1) {
-        nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN;
+        a = rand() + rand()*(1+MAX);  // Génération d'un nombre aléatoire : rand() génère un nombre aléatoire entre 0 et 32767, on le multiplie par 2 pour avoir un nombre entre 0 et 65534, on ajoute 1 pour avoir un nombre entre 1 et 65535, on multiplie par MAX pour avoir un nombre entre 1 et MAX
+        if (a < 0){
+            a=a*-1;
+        }
+        nombreAleatoire = a%MAX;
         extract_line2(temp_ligne, nombreAleatoire);
         decompose_line(temp_ligne, tab_off, temp_ligne_2);
     }
     printf("%s ", tab_off[0]);
+
+    // --------------------- // nature : Nom
     while (nature_line(tab_off) != 2) {
-        nombreAleatoire = (rand() % (MAX - MIN + 1)) + MIN;
+        a = rand() + rand()*(1+MAX);  // Génération d'un nombre aléatoire : rand() génère un nombre aléatoire entre 0 et 32767, on le multiplie par 2 pour avoir un nombre entre 0 et 65534, on ajoute 1 pour avoir un nombre entre 1 et 65535, on multiplie par MAX pour avoir un nombre entre 1 et MAX
+        if (a < 0) {
+            a = a * -1;
+        }
+        nombreAleatoire = a%MAX;
         extract_line2(temp_ligne, nombreAleatoire);
         decompose_line(temp_ligne, tab_off, temp_ligne_2);
     }
     article_line(temp_ligne_2, tab_column_3, 0);  // Vérifier le genre et le nombre
     printf("%s \n", tab_off[0]);
+    */
 
+    t_tree Verbes;
+    parcourir_le_dico(Verbes.root);
 
 
 
