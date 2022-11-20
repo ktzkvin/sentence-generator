3b1ae6ab23c3ffe26791e154b239c86d6964d6c3
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-20 23:25:23 +0100

    Update files.h

--- files.h
+++ files.h
@@ -1,14 +1,14 @@
 #ifndef SENTENCE_GENERATOR_FILES_H
 #define SENTENCE_GENERATOR_FILES_H
-#include "struct.h"
-#include "files.h"
 
-int **extract_column_1();
-int **extract_column_2();
-void article_line(char *, char *, int);
-void extract_line(char *);
-void decompose_line(char *, char **, char *);
-int nature_line(char **);
-int adj_ligne(char *, char *);
+int **extract_column_1();  // Récupère la première colonne du fichier
+int **extract_column_2();  // Récupère la deuxième colonne du fichier
+void article_line(char *, char *, int);  // Vérifie le genre et le nombre pour afficher le bon article
+void extract_line(char *);  // Récupère une ligne aléatoire du fichier
+void extract_line2(char *, int);  // Soit r un nombre aléatoire, récupère la ligne n°r du fichier
+void decompose_line(char *, char **, char *);  // Découpe une ligne en plusieurs colonnes
+int nature_line(char **);  // Récupère la nature d'une ligne
+int adj_ligne(char *, char *);  // Vérifie si l'adjectif est compatible avec le nom
+void parcourir_le_dico();  // Parcours le dictionnaire afin de créer les arbres
 
 #endif //SENTENCE_GENERATOR_FILES_H
