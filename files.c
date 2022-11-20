c11524984553577e53dad47522dc5fb8089496f6
ktzkvin <104082976+ktzkvin@users.noreply.github.com>
2022-11-20 01:25:40 +0100

    Update files.c

--- files.c
+++ files.c
@@ -102,12 +102,32 @@ void extract_line(char *tab){
     }
     // Lecture du fichier + stockage de la ligne dans un tableau de caractères 1D
     if (fileptr != NULL) {
-            fgets(tab, 100, fileptr);  // On récupère la ligne => fgets(tab à remplir, taille max de tab, fichier ouvert)
+            fgets(tab, 1287976, fileptr);  // On récupère la ligne => fgets(tab à remplir, taille max de tab, fichier ouvert)
 
     }
     // Fermeture du fichier
     fclose(fileptr);
 }
+
+
+void extract_line2(char *tab, int val){
+    FILE *fileptr;
+    int i = 0;
+    // Ouverture du fichier
+    fileptr = fopen("dico_10_lignes.txt", "r");
+    // Problème lors de la lecture du fichier
+    if (fileptr == NULL) {
+        printf("Erreur d'ouverture du fichier\n");
+    }
+    // Lecture du fichier + stockage de la ligne dans un tableau de caractères 1D
+    while (fileptr != NULL && val != i) {
+        fgets(tab, 1287976, fileptr);  // On récupère la ligne => fgets(tab à remplir, taille max de tab, fichier ouvert)
+        i++;
+    }
+    // Fermeture du fichier
+    fclose(fileptr);
+}
+
 
 void decompose_line(char *temp_ligne, char **tab_off){  // temp_ligne = tableau temporaire contenant la ligne, tab_off = tableau séparant les 3 types de la ligne
     FILE *fileptr;
@@ -138,7 +158,7 @@ void decompose_line(char *temp_ligne, char **tab_off){  // temp_ligne = tableau 
 
 int nature_line(char **tab){
     char *token;
-    token = strtok(tab[2], "\t"); // strtok permet de d'extraire une partie de la ligne par les tabulations
+    token = strtok(tab[2], ":"); // strtok permet de d'extraire une partie de la ligne par les tabulations
     if (strncmp(token, "Ver", 3) == 0){  // strncmp permet de comparer les deux chaînes de caractères
         return 1;
     }
@@ -174,70 +194,121 @@ p_cell child_horiz(p_cell cell, char letter){  // On parcourt la liste horizonta
     return new_cell;
 }
 
-void parcourir_le_dico(p_letter_node root, int type){
+
+//vérifie si la lettre est présente dans la liste des fils du noeud
+//si oui, retourne le noeud fils correspondant
+//sinon, crée un nouveau noeud fils et retourner le fils de ce noeud
+p_cell verif_present (p_letter_node cell, char elements) {
+    if (cell->son == NULL) {
+        create_son(cell, elements);
+        return cell->son;
+    } else {
+        p_letter_node temp = cell->son;
+        while (temp->son->next != NULL) {
+            if (temp->letter == elements) {
+                return temp;
+            } else {
+                temp->son->p_node = temp->son->next;
+            }
+        }
+        if (temp->letter == elements) {
+            return temp;
+        } else {
+            create_son(temp->son->next, elements);
+            return temp->son->next;
+        }
+    }
+}
+void parcourir_le_dico(p_letter_node root){
     char *dico = "dico_10_lignes.txt";
-    char **tab_off = malloc(100 * sizeof(char));  // Création d'un tableau pour séparer les 3 types d'une ligne dans un tableau en 3 lignes
-    for (int i = 0; i < 100; i++) {
+    int cpt = 0;
+    char **tab_off = malloc(300000 *
+                            sizeof(char));  // Création d'un tableau pour séparer les 3 types d'une ligne dans un tableau en 3 lignes
+    for (int i = 0; i < 300000; i++) {
         tab_off[i] = malloc(3 * sizeof(char));
     }
-    FILE *fileptr = fopen(dico,"r");
+    FILE *fileptr = fopen(dico, "r");
     // Problème lors de la lecture du fichier
     if (fileptr == NULL) {
         printf("Erreur d'ouverture du fichier\n");
-    }else{
+    } else {
         char buf[200];
-        while (fgets(buf, sizeof buf, fileptr) != NULL){
+        while (fgets(buf, sizeof buf, fileptr) != NULL) {
             decompose_line(buf, tab_off);
 
-            if ((nature_line(tab_off) == type)) {  // Création de l'arbre pour tous les types (type = 1 => verbe par ex)
-                printf("%s  ", tab_off[0]);
-                printf("%s  ", tab_off[1]);
-                printf("%s\n", tab_off[2]);
-                // Création de l'arbre
-                // si arbre vide => create_son
-                // si arbre non vide => vérifier si la lettre existe déjà dans l'arbre
-                // si la lettre existe déjà => ça renvoie le pointeur de son enfant de cette lettre => revérifier si la lettre suivante existe etc.
-                // si la lettre n'existe pas => create_son de la lettre
-
-
-                // Création de l'arbre Verbes (type = 1) avec les verbes basiques (tab_off[1] = infinitif) pour chaque ligne du dictionnaire
-                // On vérifie pour chaque lettre du mot si elle existe déjà dans l'arbre avec la fonction child_horiz qui retourne le pointeur de la lettre
-                // Si la lettre n'existe pas, child_horiz créé un fils avec cette lettre
-
-                // sachant que les structures sont :
-                //typedef struct s_letter_node t_letter_node, *p_letter_node ;
-                //
-                //typedef struct s_cell
-                //{
-                //    p_letter_node p_node ;  // Lettre du fils
-                //    struct s_cell * next ;  // Lettre à droite = frère
-                //} t_cell, *p_cell ;
-                //
-                //struct s_letter_node
-                //{
-                //    char letter ;
-                //    p_cell son ;
-                //};
-                //
-                //typedef struct s_tree
-                //{
-                //    t_letter_node *root;
-                //} t_tree, *p_tree;
-
-                // Création de l'arbre
+            if(nature_line(tab_off) == 2) {
+                // Création de l'arbre Nom
                 t_tree Noms;
-                Noms.root = malloc(sizeof(t_letter_node));
-                Noms.root->letter = tab_off[1][0];
-                Noms.root->son = NULL;
-                p_cell temp = Noms.root->son;
-                for (int i = 1; i < strlen(tab_off[1]); i++){
-                    temp = child_horiz(temp, tab_off[1][i]);
+                if (Noms.root == NULL) {
+                    Noms.root = malloc(sizeof(t_letter_node));
+                    Noms.root->letter = tab_off[1][0];
+                    Noms.root->son = NULL;
+                    cpt++;
+                } else {
+                    p_letter_node temp = Noms.root;
+                    for (int cpt; cpt < strlen(tab_off[1]); cpt++) {
+                        p_cell arbre = verif_present(temp, tab_off[1][cpt]);
+                        temp = arbre->p_node;
+                    }
                 }
-
-
+            }
+            if(nature_line(tab_off) == 3) {
+                {
+                    printf("Adjectif");
+                    // Création de l'arbre Adjectif
+                    t_tree Adjectifs;
+                    if (Adjectifs.root == NULL) {
+                        Adjectifs.root = malloc(sizeof(t_letter_node));
+                        Adjectifs.root->letter = tab_off[1][0];
+                        Adjectifs.root->son = NULL;
+                        cpt++;
+                    } else {
+                        p_letter_node temp = Adjectifs.root;
+                        for (int cpt; cpt < strlen(tab_off[1]); cpt++) {
+                            p_cell arbre = verif_present(temp, tab_off[1][cpt]);
+                            temp = arbre->p_node;
+                        }
+                    }
+                }
+            }
+            if(nature_line(tab_off) == 4) {
+                {
+                    printf("Adv");
+                    // Création de l'arbre Adverbe
+                    t_tree Adverbes;
+                    if (Adverbes.root == NULL) {
+                        Adverbes.root = malloc(sizeof(t_letter_node));
+                        Adverbes.root->letter = tab_off[1][0];
+                        Adverbes.root->son = NULL;
+                        cpt++;
+                    } else {
+                        p_letter_node temp = Adverbes.root;
+                        for (int cpt; cpt < strlen(tab_off[1]); cpt++) {
+                            p_cell arbre = verif_present(temp, tab_off[1][cpt]);
+                            temp = arbre->p_node;
+                        }
+                    }
+                }
+            }
+            if(nature_line(tab_off)==1)
+            {
+                // Création de l'arbre Verbes
+                t_tree Verbes;
+                if (Verbes.root == NULL) {
+                    Verbes.root = malloc(sizeof(t_letter_node));
+                    Verbes.root->letter = tab_off[1][0];
+                    Verbes.root->son = NULL;
+                    cpt++;
+                } else {
+                    p_letter_node temp = Verbes.root;
+                    for (int cpt; cpt < strlen(tab_off[1]); cpt++) {
+                        p_cell arbre = verif_present(temp, tab_off[1][cpt]);
+                        temp = arbre->p_node;
+                    }
+                }
+                display_tree(Verbes);
             }
         }
     }
     fclose(fileptr);
 }
-
